9d5d0afaa9df77b663814068f38a96693122f81e
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-03 12:29:37 +0500

    HeroBanner fixed

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import Link from "next/link";
 export default function HeroBanner() {
   return (
-    <>
-      <div className="hero-banner-container">
+    <div className="hero-banner-container">
+      <div>
         <p className="beats-solo">SMALL TEXT</p>
         <h3>MID TEXT</h3>
+        <h1>largeText1</h1>
         <img src="" alt="headphones" className="hero-banner-image" />
-      </div>
-      <div>
-        <Link href="/product/ID">
-          <button type="button">BUTTON TEXT</button>
-        </Link>
-        <div className="desc">
-          <h5>Description</h5>
-          <p>DESCRIPTION</p>
+
+        <div>
+          <Link href={`/product/id`}>
+            <button type="button">Button</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, possimus.</p>
+          </div>
         </div>
       </div>
-    </>
-  );
+    </div>
+  )
 }
