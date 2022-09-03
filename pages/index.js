37d7fc730b7468ddfca15569b08803e027996f85
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-03 14:58:27 +0500

    Fetch data from Sanity

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,32 @@
 import React from "react";
-import {FooterBanner,Product,HeroBanner} from './../components'
-export default function index() {
+import { FooterBanner, Product, HeroBanner } from "./../components";
+import { client } from "../lib/client";
+const index = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner/>
-      <div  className="products-heading">
-      <h2>Best Selling products</h2>
-      <p>Speaker of many varitions</p>
-</div>
-      <div  className="products-container">
-        {["product 1", "product 2"].map((product) =>  product
-        )}
+      <HeroBanner />
+      <div className="products-heading">
+        <h2>Best Selling products</h2>
+        <p>Speaker of many varitions</p>
       </div>
-      <FooterBanner/>
+      <div className="products-container">
+        {products.map((product) => product.name)}
+      </div>
+      <FooterBanner />
     </>
   );
-}
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default index;
