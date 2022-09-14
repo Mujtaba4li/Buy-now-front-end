aeb279758186e31e464f5e8617d3df19c2cbacf1
Mujtaba Ali <mujiroy01@gmail.com>
2022-09-14 11:36:28 +0500

    Implemented Context'Single source of all data'

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,34 +1,41 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
+
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  }
 
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img
-              src={urlFor(image && image[0])}
-              className="product-detail-image"
-            />
+            <img src={urlFor(image && image[index])} className="product-detail-image" />
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
-              <img
+              <img 
                 key={i}
                 src={urlFor(item)}
-                className={
-                  i === 0 ? "small-image selected-image" : "small-image"
-                }
+                className={i === index ? 'small-image selected-image' : 'small-image'}
                 onMouseEnter={() => setIndex(i)}
               />
             ))}
           </div>
         </div>
 
-        {/* <div className="product-detail-desc">
+        <div className="product-detail-desc">
           <h1>{name}</h1>
           <div className="reviews">
             <div>
@@ -57,34 +64,44 @@ const ProductDetails = ({ product, products }) => {
             <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
             <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button>
           </div>
-        </div> */}
+        </div>
+      </div>
+
+      <div className="maylike-products-wrapper">
+          <h2>You may also like</h2>
+          <div className="marquee">
+            <div className="maylike-products-container track">
+              {products.map((item) => (
+                <Product key={item._id} product={item} />
+              ))}
+            </div>
+          </div>
       </div>
     </div>
-  );
-};
-
+  )
+}
 
 export const getStaticPaths = async () => {
-    const query = `*[_type == "product"] {
-      slug {
-        current
-      }
-    }
-    `;
-  
-    const products = await client.fetch(query);
-  
-    const paths = products.map((product) => ({
-      params: { 
-        slug: product.slug.current
-      }
-    }));
-  
-    return {
-      paths,
-      fallback: 'blocking'
+  const query = `*[_type == "product"] {
+    slug {
+      current
     }
   }
+  `;
+
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: { 
+      slug: product.slug.current
+    }
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
 
 export const getStaticProps = async ({ params: { slug }}) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
@@ -99,4 +116,5 @@ export const getStaticProps = async ({ params: { slug }}) => {
     props: { products, product }
   }
 }
-export default ProductDetails;
+
+export default ProductDetails
